ec7fcbded04baeaf8c79748abd26de9b2b6cd97d
Gall-oDrone <piehavok@hotmail.com>
2022-11-20 12:09:11 -0600

    Change the Message text

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Vah ih ah Palatio?"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
